4cc5f3f5fc470ba24f165a777f2774637fc23ca9
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-09-11 18:53:06 +0500

    rename login route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { ROUTE_USER } from "../constants/routes.js";
+import { ROUTE_LOGIN } from "../constants/routes.js";
 import UserController from "../controllers/UserController.js";
 
 const routerUser = new Router();
 const userController = new UserController();
 
-routerUser.post(ROUTE_USER, (req, res) => userController.loginUser(req, res));
+routerUser.post(ROUTE_LOGIN, (req, res) => userController.loginUser(req, res));
 
 export default routerUser;
